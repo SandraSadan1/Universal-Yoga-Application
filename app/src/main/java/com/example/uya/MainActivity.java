73e7c769c3ce306691181517e9a7eec36896046b
SandraSadan1 <ss5282g@gre.ac.uk>
2023-11-07 21:56:15 +0000

    Initial commit

--- app/src/main/java/com/example/uya/MainActivity.java
+++ app/src/main/java/com/example/uya/MainActivity.java
@@ -3,30 +3,57 @@ package com.example.uya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.TimePickerDialog;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
-import java.util.Calendar;
 import android.widget.Toast;
+import android.util.Log;
+import com.example.uya.model.YogaCourse;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText startTime;
     private EditText endTime;
-    private Spinner dayOfWeek;
     private EditText capacity;
     private EditText duration;
     private EditText pricePerClass;
+    private Spinner dayOfWeek;
+    private Spinner yogaType;
+    private EditText description;
+    private static final String ID = "id";
+    private static final String DAY = "day";
+    private static final String DURATION = "duration";
+    private static final String CAPACITY = "capacity";
+    private static final String PRICE = "price";
+    private static final String START_TIME = "start_time";
+    private static final String END_TIME = "end_time";
+    private static final String YOGA_TYPE = "yoga_type";
+    private static final String DESCRIPTION = "description";
 
     private Button submitButton;
+    private static final String TAG = "Universal Yoga Application";
+    private MyDatabaseHelper dbHelper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the database helper with the application context
+        dbHelper = new MyDatabaseHelper(this);
+        db = dbHelper.getWritableDatabase(); // Open the database for writing
 
         dayOfWeek = findViewById(R.id.dayOfWeek);
         startTime = findViewById(R.id.startTime);
@@ -34,21 +61,13 @@ public class MainActivity extends AppCompatActivity {
         capacity = findViewById(R.id.capacity);
         duration = findViewById(R.id.duration);
         pricePerClass = findViewById(R.id.pricePerClass);
+        description = findViewById(R.id.description);
+        yogaType = findViewById(R.id.typeOfClass);
 
-
-        startTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showTimePickerDialog(startTime);
-            }
-        });
-
-        endTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showTimePickerDialog(endTime);
-            }
-        });
+        startTime.setOnClickListener(view -> showTimePickerDialog(startTime));
+        endTime.setOnClickListener(view -> showTimePickerDialog(endTime));
+        submitButton = findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(view -> validateAndSubmitForm());
     }
 
     private void showTimePickerDialog(final EditText editText) {
@@ -57,52 +76,122 @@ public class MainActivity extends AppCompatActivity {
         int minute = currentTime.get(Calendar.MINUTE);
 
         TimePickerDialog timePickerDialog = new TimePickerDialog(this,
-                new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        String time = String.format("%02d:%02d", selectedHour, selectedMinute);
-                        editText.setText(time);
-                    }
+                (timePicker, selectedHour, selectedMinute) -> {
+                    String time = String.format("%02d:%02d", selectedHour, selectedMinute);
+                    editText.setText(time);
+                    editText.setError(null);
                 }, hour, minute, true);
 
         timePickerDialog.show();
     }
 
-    private void validateAndSubmitForm() {
-
-        boolean isValid = true;
-
-         //Check if each field is empty
-        if (startTime.getText().toString().trim().isEmpty()) {
-            startTime.setError("Start Time is required");
-            isValid = false;
-      }
-
-       if (endTime.getText().toString().trim().isEmpty()) {
-        endTime.setError("End Time is required");
-            isValid = false;
-        }
-        if (capacity.getText().toString().trim().isEmpty()) {
-            capacity.setError("Capacity is required");
-            isValid = false;
-        }
-        if (duration.getText().toString().trim().isEmpty()) {
-            duration.setError("Duration is required");
-            isValid = false;
-        }
-        if (pricePerClass.getText().toString().trim().isEmpty()) {
-            pricePerClass.setError("price is required");
-            isValid = false;
-        }
-        // Add more field validation checks for other form fields as needed
-
-        if (isValid) {
-            // All fields have values, proceed with form submission
-            // Implement your submission logic here
-            // For example, you can send the form data to a server or perform some action
-            Toast.makeText(this, "Form submitted successfully", Toast.LENGTH_SHORT).show();
+    private void clearAllEditTextFields() {
+        EditText[] fields = {startTime, endTime, capacity, duration, pricePerClass, description};
+        for (EditText field : fields) {
+            field.setText("");
         }
     }
 
+    private void validateAndSubmitForm() {
+        boolean isValid = true;
+        EditText[] fields = {startTime, endTime, capacity, duration, pricePerClass};
 
+        for (EditText field : fields) {
+            String fieldValue = field.getText().toString().trim();
+
+            if (fieldValue.isEmpty()) {
+                field.setError("This field is required");
+                isValid = false;
+            } else {
+                field.setError(null);
+            }
+        }
+
+        if (isValid) {
+            ContentValues values = new ContentValues();
+            values.put(DAY, dayOfWeek.getSelectedItem().toString());
+            values.put(DURATION, duration.getText().toString());
+
+            String priceValue = pricePerClass.getText().toString().trim();
+            if (!priceValue.isEmpty()) {
+                try {
+                    int price = Integer.parseInt(priceValue);
+                    values.put(PRICE, price);
+                } catch (NumberFormatException e) {
+                    pricePerClass.setError("Invalid price format");
+                    isValid = false;
+                }
+            }
+
+            values.put(START_TIME, startTime.getText().toString());
+            values.put(END_TIME, endTime.getText().toString());
+            values.put(YOGA_TYPE, yogaType.getSelectedItem().toString());
+            values.put(CAPACITY, capacity.getText().toString());
+            values.put(DESCRIPTION, description.getText().toString());
+
+            if (isValid) {
+                try {
+                    long newRowId = db.insert("course_details", null, values);
+                    if (newRowId != -1) {
+                        clearAllEditTextFields(); // Clear fields after successful submission
+                        Toast.makeText(this, "Course added successfully", Toast.LENGTH_SHORT).show();
+                        getAllYogaCourses();
+                    } else {
+                        Log.d(TAG, "Insertion failed");
+                    }
+                } catch (SQLException e) {
+                    Log.e(TAG, "Database error: " + e.getMessage());
+                    // Handle the error
+                }
+            }
+        }
+    }
+
+    public List<YogaCourse> getAllYogaCourses() {
+        List<YogaCourse> yogaCourses = new ArrayList<>();
+
+        String[] columns = {ID, DAY, DURATION, PRICE, START_TIME, END_TIME, YOGA_TYPE, CAPACITY, DESCRIPTION};
+        String selection = null;
+        String[] selectionArgs = null;
+
+        try {
+            Cursor cursor = db.query("course_details", columns, selection, selectionArgs, null, null, null);
+
+            if (cursor != null && cursor.moveToFirst()) {
+                do {
+                    // Retrieve data from the Cursor and create a YogaCourse object
+                    int id = cursor.getInt(cursor.getColumnIndex(ID));
+                    String day = cursor.getString(cursor.getColumnIndex(DAY));
+                    String duration = cursor.getString(cursor.getColumnIndex(DURATION));
+                    int price = cursor.getInt(cursor.getColumnIndex(PRICE));
+                    String startTime = cursor.getString(cursor.getColumnIndex(START_TIME));
+                    String endTime = cursor.getString(cursor.getColumnIndex(END_TIME));
+                    String yogaType = cursor.getString(cursor.getColumnIndex(YOGA_TYPE));
+                    String capacity = cursor.getString(cursor.getColumnIndex(CAPACITY));
+                    String desc = cursor.getString(cursor.getColumnIndex(DESCRIPTION));
+
+                    YogaCourse yogaCourse = new YogaCourse(id, day, duration, price, startTime, endTime, yogaType, capacity, desc);
+                    yogaCourses.add(yogaCourse);
+                    // Display the data in the log
+                    Log.d("Data", "ID: " + id + ", Day: " + day + ", Duration: " + duration + ", Price: " + price + ", Start Time: " + startTime + ", End Time: " + endTime + ", Yoga Type: " + yogaType + ", Capacity: " + capacity + ", Description: " + description);
+                } while (cursor.moveToNext());
+
+                cursor.close();
+            } else {
+                Log.d(TAG, "Cursor is null or empty");
+            }
+        } catch (SQLException e) {
+            Log.e(TAG, "Database error: " + e.getMessage());
+            // Handle the error
+        }
+
+        return yogaCourses;
+    }
+
+    @Override
+    protected void onDestroy() {
+        db.close();
+        dbHelper.close(); // Close the database
+        super.onDestroy();
+    }
 }
---
+++ app/src/main/java/com/example/uya/MyDatabaseHelper.java
@@ -0,0 +1,62 @@
+package com.example.uya;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "UniversalYogaApp";
+    private static final int DATABASE_VERSION = 13;
+
+    // Table and column names
+    private static final String TABLE_NAME = "course_details";
+    private static final String ID = "id";
+    private static final String DAY = "day";
+    private static final String DURATION = "duration";
+    private static final String CAPACITY = "capacity";
+    private static final String PRICE = "price";
+    private static final String START_TIME = "start_time";
+    private static final String END_TIME = "end_time";
+    private static final String YOGA_TYPE = "yoga_type";
+    private static final String DESCRIPTION = "description";
+
+    // Define an array of column names for easy access
+    private static final String[] COLUMNS = {
+            ID, DAY, DURATION, CAPACITY, PRICE, START_TIME, END_TIME, YOGA_TYPE, DESCRIPTION
+    };
+
+    // Define SQL statements as constants
+    private static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    DAY + " TEXT, " +
+                    DURATION + " TEXT, " +
+                    PRICE + " INTEGER, " +
+                    START_TIME + " TEXT, " +
+                    END_TIME + " TEXT, " +
+                    YOGA_TYPE + " TEXT, " +
+                    CAPACITY + " TEXT, " +
+                    DESCRIPTION + " TEXT" +
+                    ")";
+
+    public MyDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d("MyDatabaseHelper", "onCreate method is executed");
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion < newVersion) {
+            // Drop the old table (if it exists)
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            // Recreate the table with the new schema
+            onCreate(db);
+        }
+    }
+}
