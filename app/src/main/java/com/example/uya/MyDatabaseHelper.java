c78fddf487c95869791d3442d7b7cf4a4898eb42
sneha k salu <sk5273y@gre.ac.uk>
2023-11-14 19:43:04 +0000

    Edit courses

--- app/src/main/java/com/example/uya/MyDatabaseHelper.java
+++ app/src/main/java/com/example/uya/MyDatabaseHelper.java
@@ -4,6 +4,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String TAG = "MyDatabaseHelper";
     public static final String DATABASE_NAME = "UniversalYogaApp";
     public static final int DATABASE_VERSION = 20;
     // Course details table and column names
