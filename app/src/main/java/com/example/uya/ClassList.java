805798facd90b38418f81351934b8305c25b6b4d
MaryIvaniya <ivaniyajoy@gmail.com>
2023-11-21 11:20:27 +0000

    Class list deletion

--- app/src/main/java/com/example/uya/ClassList.java
+++ app/src/main/java/com/example/uya/ClassList.java
@@ -1,19 +1,26 @@
 package com.example.uya;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.uya.model.YogaClass;
@@ -95,16 +102,67 @@ public class ClassList extends AppCompatActivity {
                 commentsTextView.setText(yogaClass.getComments());
             }
 
+            LinearLayout actionLayout = new LinearLayout(this);
+            actionLayout.setOrientation(LinearLayout.HORIZONTAL);
+            actionLayout.setGravity(Gravity.CENTER);
+
+            ImageView deleteImageView = new ImageView(this);
+            deleteImageView.setImageResource(R.drawable.delete);
+            deleteImageView.setLayoutParams(new ViewGroup.LayoutParams(52, 52));
+            actionLayout.addView(deleteImageView);
+
+            // Get the class ID
+            final int classId = yogaClass.getId();
+            // Set click listener for delete icon
+            deleteImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Create and show an AlertDialog to confirm deletion
+                    new AlertDialog.Builder(ClassList.this)
+                            .setTitle("Confirm Deletion")
+                            .setMessage("Are you sure you want to delete this Yoga class?")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    SQLiteDatabase db = databaseHelper.getWritableDatabase();
+                                    try {
+                                        // Execute the DELETE statement
+                                        db.delete("yoga_class", MyDatabaseHelper.ID + "=?", new String[]{String.valueOf(classId)});
+                                        showToast("Class deleted successfully");
+                                        // After deletion, refresh the table
+                                        populateTable(dayOfWeekSpinner.getSelectedItem().toString());
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    } finally {
+                                        db.close();
+                                    }
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // User clicked "No," do nothing, or you can add additional logic
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
+            });
+
             // Add TextViews to the TableRow
             tableRow.addView(dateTextView);
             tableRow.addView(timeTextView);
             tableRow.addView(teacherTextView);
             tableRow.addView(commentsTextView);
+            tableRow.addView(actionLayout);
 
             // Add the TableRow to the existing TableLayout
             tableLayout.addView(tableRow);
         }
     }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
     public void navigateToHome(View view) {
         Intent intent = new Intent(this, HomeActivity.class); // Replace HomeActivity with the name of your home screen activity
         startActivity(intent);
